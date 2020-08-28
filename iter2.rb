c5e270e3890ca6a37cfb97042558d055aea482d2
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2020-08-27 20:51:32 -0400

    escape

---
+++ iter2.rb
@@ -0,0 +1,26 @@
+ventas = {
+    Enero:15000,
+    Febrero:22000,
+    Marzo:12000,
+    Abril:17000,
+    Mayo:81000,
+    Junio:13000,
+    Julio:21000,
+    Agosto:41200,
+    Septiembre:25000,
+    Octubre:21500,
+    Noviembre:91000,
+    Diciembre:21000
+
+}
+
+ventas.each do | key, value |
+    print "#{key} "if value > 45000
+end 
+
+
+
+# si esta en comilla es string,  sino es simbolo. 
+
+# print ventas
+
---
+++ iter3.rb
@@ -0,0 +1,26 @@
+ventas = {
+    Enero:15000,
+    Febrero:22000,
+    Marzo:12000,
+    Abril:17000,
+    Mayo:81000,
+    Junio:13000,
+    Julio:21000,
+    Agosto:41200,
+    Septiembre:25000,
+    Octubre:21500,
+    Noviembre:91000,
+    Diciembre:21000
+    }
+
+def filter(ventas, num)
+    filtered_ventas = {} 
+    ventas.each do |meses,precio|
+        if precio > num
+            filtered_ventas[meses] = precio
+        end 
+    end 
+    return filtered_ventas
+end 
+puts filter(ventas, 45000)
+
